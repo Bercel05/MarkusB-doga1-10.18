db80c5ef7b8a4cea27c53a5351b2a4d40df1f185
MarkusBercel <markusb@kkszki.hu>
2024-10-18 10:12:00 +0200

    The exercise is ready

--- Program.cs
+++ Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace doga1
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+       
+        Console.Write("Kérem, adja meg a vásárlási összeget (Ft): ");
+        double vasarlasiOsszeg = Convert.ToDouble(Console.ReadLine());
+
+       
+        Console.Write("Van-e törzsvásárlói kártyája? (igen/nem): ");
+        string torzsvasarloiKartya = Console.ReadLine().Trim().ToLower();
+
+       
+        double fizetendoOsszeg = vasarlasiOsszeg;
+
+       
+        if (vasarlasiOsszeg < 10000)
         {
+           
+            fizetendoOsszeg = vasarlasiOsszeg;
         }
+        else if (vasarlasiOsszeg >= 10000 && vasarlasiOsszeg <= 50000)
+        {
+            
+            if (torzsvasarloiKartya == "igen")
+            {
+                fizetendoOsszeg *= 0.95;
+            }
+        }
+        else
+        {
+            
+            if (torzsvasarloiKartya == "igen")
+            {
+                fizetendoOsszeg *= 0.90; 
+            }
+            else
+            {
+                fizetendoOsszeg *= 0.95;
+            }
+        }
+
+        Console.WriteLine($"Fizetendő összeg: {fizetendoOsszeg:0} Ft");
     }
 }
